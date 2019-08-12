744fb9b7a2365fb60661bf61546d4447e9e171e9
tejasbandawane <tejasbandawane5@gmail.com>
2019-08-12 11:15:17 +0530

    1st Program in Conditional

---
+++ src/com/bridgelabz/conditionaljavaprogram/FlipCoin.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+ *  
+ *  Purpose: Flip a coin - Heads or Tails
+ *
+ *  @author  rahul
+ *  @version 1.0
+ *  @since   12-08-2019
+ *
+ ******************************************************************************/
+
+package com.bridgelabz.conditionaljavaprogram;
+
+import com.bridgelabz.utility.Utils;
+
+public class FlipCoin {
+
+	public static void main(String[] args) {
+		
+		Utils utility = new Utils();
+		
+		utility.flipCoin();
+		
+	}
+
+}
--- src/com/bridgelabz/utility/Utils.java
+++ src/com/bridgelabz/utility/Utils.java
@@ -1,5 +1,28 @@
+/******************************************************************************
+ *  
+ *  Purpose: The Utility file to store the logic of all the program.
+ *
+ *  @author  rahul
+ *  @version 1.0
+ *  @since   12-08-2019
+ *
+ ******************************************************************************/
+
 package com.bridgelabz.utility;
 
 public class Utils {
 
+	
+	/**
+	 *  Purpose:- Flip a Coin - Head or Tails
+	 */
+	public void flipCoin() {
+		
+		if(Math.random() < 0.5)
+			System.out.println("Heads");
+		else
+			System.out.println("Tails");
+		
+	}
+	
 }
